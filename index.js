0ef00fa8487ea8b82d14020c98b268e306d39c09
Nina Adora <nirinakaes@gmail.com>
2026-03-25 15:29:49 +0300

    Implement function stubs for tax calculation, string conversion, maximum value finding, palindrome checking, and discounted price calculation

--- index.js
+++ index.js
@@ -5,3 +5,36 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+//1.Calculate Tax
+
+function calculateTax(){
+
+}
+
+//2.Convert to uppercase
+
+function convertToUpperCase (){
+
+
+}
+
+//3.Finding the maximum
+function findMaximum(){
+
+
+
+}
+//4.
+function isPalindrome (){
+
+
+
+}
+
+//5.Calculate Discount
+
+function calculateDiscountedPrice(){
+
+
+}
